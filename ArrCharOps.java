e755de659a76c2aa75aefffb2b981a5bb74f9812
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-24 15:19:31 +0200

    ArrCharOps implementation

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -197,7 +197,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
             public static int compareTo(String str1, String str2) {
-                if (str1 == null || str2 == null) return -2;
+                if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) return -2;
 
                 char[] arr1 = str1.toCharArray();
                 char[] arr2 = str2.toCharArray();
