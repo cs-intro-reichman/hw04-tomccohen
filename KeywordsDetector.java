fc166103280ed83df97cf8c180ff9e1910b3987f
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-11-26 17:11:07 +0200

    KeywordsDetector implementation

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,21 +21,34 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        //boolean contains = false;
 
-        for (int i = 0; i < keywords.length; i++)
+        for (int i = 0; i < sentences.length; i++) // loop of the sentences
         {
-            String word = keywords[i];
-            
-            for (int j = 0; j < sentences.length; j++)
+            String currentSentnce = sentences[i];
+            String tempS = currentSentnce.toLowerCase();
+
+            for (int j = 0; j < keywords.length; j++) // loop of the words
             {
-                String sentence = sentences[j];
-                if (MyString.contains(sentence, word))
+                boolean contains = false;
+                String currentWord = keywords[j];
+                String tempW = currentWord.toLowerCase();
+                int lastIndex = currentSentnce.length() - currentWord.length() + 1;
+
+                for (int k = 0; k < lastIndex && !contains; k++) // loop of every char of the word in the sentncese
                 {
-                    System.out.println(sentence);
+                    if (tempW.charAt(0) == tempS.charAt(k))
+                        {
+                            contains = true;
+                            for (int l = 0; l <  currentWord.length() && contains; l ++)
+                            {
+                                contains = tempW.charAt(l) == tempS.charAt(l + k);
+                            }
+                        }
+         
                 }
-            }
+                if (contains) System.out.println(currentSentnce);
+            }     
+            
         }
-        
     }
 }
